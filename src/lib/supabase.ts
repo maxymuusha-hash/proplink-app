d727cc04de880f36a11129560a8f0e9b7fa0d974
maxymuusha-hash <maxymuusha@gmail.com>
2026-05-28 17:23:12 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 // Initialize database client
-const supabaseUrl = 'https://qukkolsfjxptxktgcvjt.databasepad.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjI5MDRmNWZiLWY5MDctNGJiNC1iMDFmLTY0MTllZWY4ZWM1MiJ9.eyJwcm9qZWN0SWQiOiJxdWtrb2xzZmp4cHR4a3RnY3ZqdCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzY2MjUxMzgzLCJleHAiOjIwODE2MTEzODMsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.uz_BCa63l6N1MYj4J8J-cVbcUdAYsAb8OzUxv_4KCmE';
+const supabaseUrl = 'https://rgrntdinnwywkvovmzyz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJncm50ZGlubnd5d2t2b3Ztenl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NzIzODIsImV4cCI6MjA5NTU0ODM4Mn0.yb5yTBN48Du_hucF3-vvAFCexmpXlA6E-MK_k6wVvUg';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
